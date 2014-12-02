796252675768ec0b0dc76b12d5991f42ac1bae26
namdw <dwn28@cornell.edu>
2014-12-02 00:46:11 -0500

    NB modified

--- nb.py
+++ nb.py
@@ -6,7 +6,6 @@ Created on Nov 13, 2014
 
 import math 
 from Datapoint import Datapoint
-import numpy as np
 
 def nb(traindata, testdata):
     
@@ -39,32 +38,37 @@ def nb(traindata, testdata):
 
     results = []
     for testcase in testfeature:
+        # getting a result for a single test case. If there is only one test case, it will only run once.
         probs = []
         for label in labels:
+            # find probaility of each label and store the result 
             nlabel = len(labelindex[labels.index(label)])
             PY = nlabel/len(trainlabel)
             index = labelindex(labels.index(label))
             nbprob = PY
-            newfeatures = [[]*len(testcase)]
+            newfeatures = [[]]
+            # change the direction of the feature matrix of the specific label case
+            for i in range(len(testcase)):
+                newfeatures.append([])
             for idx in index:
                 testdata = trainfeature[idx]
                 for i in range(len(testdata)):
                     newfeatures[i].append(testdata[i])
+            # find PXY for each feature and mutiply to the final probability to find the prob of each label
             for i in range(len(testcase)):
                 feature = testcase[i]
                 featurepool = newfeatures[i]
                 PXY = (featurepool.count(feature)+1)/(len(featurepool)+len(set(featurepool)))
                 nbprob = nbprob * PXY
+            # storing the probability of each label cases
             probs.append(nbprob)
+        # determine the label with maximum probability and add to results
         maxprob = max(probs)
         results.append(labels(probs.index(maxprob)))
+    # result contains the max probability label for each test case
+    print accuracy(results,testlabel)
     return results
 
-
-
-
-        
-    
 def accuracy(result, data):
     match = 0
     for i in range(len(result)):
@@ -72,26 +76,9 @@ def accuracy(result, data):
             match = match+1
     return match/len(result)
 
-def nbprob(pwy,py,feature):
-    problist = []
-    for i in range(len(feature)):
-        problist.append(math.log(math.pow(pwy[i],feature[i])))
-    return sum(problist + math.log(py))
-    
-def matdiv(mat1,mat2):
-    result = []
-    for i in range(len(mat1)):
-        result.append(mat1[i]/mat2[i])
-    return result
-
 def find(mat,element):
     idx = []
     for i in range(len(mat)):
         if mat(i)==element:
             idx.append(i)
     return idx
-    
-    
-# if __name__ == '__main__':
-#     nb(data)
-#     pass
