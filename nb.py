e3fb291ff4e67d5e6e31d7bad3e9a4125ec9540a
namdw <dwn28@cornell.edu>
2014-12-02 22:10:28 -0500

    working nb

--- nb.py
+++ nb.py
@@ -35,38 +35,39 @@ def nb(traindata, testdata):
 
     labels = list(set(trainlabel))
     labelindex = []
-    for label in labels:
+    for i in range(len(labels)):
+        label = labels[i]
         labelindex.append(find(trainlabel,label))
-
     results = []
+    count = 1
     for testcase in testfeature:
+        print 'testcase',count
+        count = count + 1
         # getting a result for a single test case. If there is only one test case, 
         # it will only run once.
         probs = []
         for label in labels:
             # find probaility of each label and store the result 
             nlabel = len(labelindex[labels.index(label)])
-            PY = nlabel/len(trainlabel)
+            PY = float(nlabel)/len(trainlabel)
             index = labelindex[labels.index(label)]
+            index = index[0:30]
             nbprob = PY
             newfeatures = [[]]
             # change the direction of the feature matrix of the specific label case
-            for i in range(len(testcase)-1):
+            for j in range(len(testcase)):
                 newfeatures.append([])
             for idx in index:
                 testdata = trainfeature[idx]
-                for i in range(len(testdata)):
-                    newfeatures[i].append(testdata[i])
-            # print newfeatures
-            # print 'len newfeatures ',len(newfeatures[0])
-            # print len(trainfeature[0])
+                for j in range(len(testdata)):
+                    newfeatures[j].append(testdata[j])
             # find PXY for each feature and mutiply to the final probability 
             # to find the prob of each label
-            for i in range(len(testcase)):
-                feature = testcase[i]
-                featurepool = newfeatures[i]
+            for j in range(len(testcase)):
+                feature = testcase[j]
+                featurepool = newfeatures[j]
                 # print featurepool
-                PXY = (featurepool.count(feature)+1)/(len(featurepool)+len(set(featurepool)))
+                PXY = float(featurepool.count(feature)+1)/(len(featurepool)+len(set(featurepool)))
                 nbprob = nbprob * PXY
             # storing the probability of each label cases
             probs.append(nbprob)
@@ -75,15 +76,14 @@ def nb(traindata, testdata):
         results.append(labels[probs.index(maxprob)])
     # result contains the max probability label for each test case
     print 'acc: ', accuracy(results,testlabel)
-    print results
     return results
 
 def accuracy(result, data):
     match = 0
     for i in range(len(result)):
-        if result[i]==data[i]:
+        if result[i]-data[i]==0:
             match = match+1
-    return match/len(result)
+    return float(match)/len(result)
 
 def find(mat,element):
     idx = []
