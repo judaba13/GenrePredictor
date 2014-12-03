6eeb382a45d080345e4746aa28eb2d79e3ea955e
Matthew Hayes <mjh388@cornell.edu>
2014-12-03 14:42:02 -0500

    fam vs hot graph

---
+++ famhotgrpah.py
@@ -0,0 +1,22 @@
+from numpy import *
+from matplotlib import pyplot as plt
+import csv
+
+data = csv.reader(open('famhot.csv'))
+data = array([ x for x in data]).astype('float')
+fam = [d[0] for d in data]
+hot = [d[1] for d in data]
+plt.figure()
+plt.title("hotttnesss vs familiarity")
+plt.xlabel("familiarity")
+plt.ylabel("hotttnesss")
+plt.plot(fam, hot, '.')
+fThreshx = [0.6,0.6]
+fThreshy = [0, 1]
+hThreshx = [0, 1]
+hThreshy = [0.425, 0.425]
+
+plt.plot(fThreshx, fThreshy, 'r-')
+plt.plot(hThreshx, hThreshy, 'r-')
+plt.show()
+plt.savefig("famhotgraph.png")
