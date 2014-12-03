163429684fdcf73f9abbb5cec3ffc7e440762f00
Justin Bard <jdb387@cornell.edu>
2014-12-02 20:34:21 -0500

    changed DataPoint class in Artistdata.py to have label field now

--- Artistdata.py
+++ Artistdata.py
@@ -1,3 +1,6 @@
+HOT_THRESH = 0.425
+FAM_THRESH = 0.6
+
 class DataPoint(dict):
 	def __init__(self, other):
 		dict.__init__(self, other)
@@ -8,6 +11,7 @@ class DataPoint(dict):
 		self.track_ids = self['track_id']
 		self.artist_name = self['artist_name']
 		self.years = self['years']
+		self.label = 1 if self.hotttnesss >= HOT_THRESH and self.familiarity <= FAM_THRESH else -1
 		del self['years']
 		del self['track_id']
 		del self['artist_name']
@@ -15,6 +19,9 @@ class DataPoint(dict):
 		del self['artist_location']
 		del self['artist_familiarity']
 		del self['artist_hotttnesss']
+
+	def __sub__(self, other):
+		return Datapoint({x: self[x]-other[x] for x in self if other[x]!=None})
 
 """
 for d in data: # datapoint d, data as a list of Datapoint
